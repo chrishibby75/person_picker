7971bf30a3c7f81d8e8738bb5c90a83cc8d779b5
chrishibby75 <chrishibby75@gmail.com>
2020-06-19 23:02:21 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function Member (name, src) {
+    this.name = name;
+    this.src = src;
+}
+var shawntell = new Member("Shawntell Maez", "images/shawncy.jpg");
+var matt = new Member("Matt Palmer", "images/matt.jpg");
+var jordan = new Member("Jordan Parr", "images/jordan.jpg");
+var chris = new Member("Chris Hibberd", "images/chris.jpg");
+var david = new Member("David Orme", "images/david.jpg");
+var jenn = new Member("Jenn Pickett", "images/jenn.jpg");
+var cutler = new Member("Cutler Christensen", "images/cutler.jpg");
+var ashley = new Member("Ashley Dawson", "images/ashley.jpg");
+
+var members = [shawntell, matt, jordan, chris, david, jenn, cutler, ashley]
+
+$("#random-button").on("click", function() {
+    var randomMember = members[Math.floor(Math.random() * members.length)];
+    console.log(randomMember.name);
+
+    $("#name-field").html("<h1>" + randomMember.name + "</h1>");
+    $("#img-field").attr("src", randomMember.src);
+})
