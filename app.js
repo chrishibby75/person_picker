e51595b875108218389906f43fc17fbb82b3cd59
chrishibby75 <chrishibby75@gmail.com>
2020-06-20 10:59:44 -0600

    added member

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ var david = new Member("David Orme", "images/david.jpg");
 var jenn = new Member("Jenn Pickett", "images/jenn.jpg");
 var cutler = new Member("Cutler Christensen", "images/cutler.jpg");
 var ashley = new Member("Ashley Dawson", "images/ashley.jpg");
+var brice = new Member("Brice Gibson", "images/brice.jpg");
 
-var members = [shawntell, matt, jordan, chris, david, jenn, cutler, ashley]
+var members = [shawntell, matt, jordan, chris, david, jenn, cutler, ashley, brice]
 
 $("#random-button").on("click", function() {
     var randomMember = members[Math.floor(Math.random() * members.length)];
-    console.log(randomMember.name);
+    // console.log(randomMember.name);
 
     $("#name-field").html("<h1>" + randomMember.name + "</h1>");
     $("#img-field").attr("src", randomMember.src);
-})
+
+    // members.splice(randomMember, 1);
+    // console.log(members);
+});
