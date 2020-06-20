7dc91692a72f3a5ecadc7caae0ed5bdb0bac7a2e
chrishibby75 <chrishibby75@gmail.com>
2020-06-20 13:17:59 -0600

    refractor img

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ function Member (name, src) {
     this.name = name;
     this.src = src;
 }
-var shawntell = new Member("Shawntell Sanchez", "images/shawncy.jpg");
-var matt = new Member("Matt Palmer", "images/matt.jpg");
-var jordan = new Member("Jordan Parr", "images/jordan.jpg");
-var chris = new Member("Chris Hibberd", "images/chris.jpg");
-var david = new Member("David Orme", "images/david.jpg");
-var jenn = new Member("Jenn Pickett", "images/jenn.jpg");
-var cutler = new Member("Cutler Christensen", "images/cutler.jpg");
-var ashley = new Member("Ashley Dawson", "images/ashley.jpg");
-var brice = new Member("Brice Gibson", "images/brice.jpg");
+var shawntell = new Member("Shawntell Sanchez", "shawncy.jpg");
+var matt = new Member("Matt Palmer", "matt.jpg");
+var jordan = new Member("Jordan Parr", "jordan.jpg");
+var chris = new Member("Chris Hibberd", "chris.jpg");
+var david = new Member("David Orme", "david.jpg");
+var jenn = new Member("Jenn Pickett", "jenn.jpg");
+var cutler = new Member("Cutler Christensen", "cutler.jpg");
+var ashley = new Member("Ashley Dawson", "ashley.jpg");
+var brice = new Member("Brice Gibson", "brice.jpg");
 
 var members = [shawntell, matt, jordan, chris, david, jenn, cutler, ashley, brice]
 
@@ -19,8 +19,5 @@ $("#random-button").on("click", function() {
     // console.log(randomMember.name);
 
     $("#name-field").html("<h1>" + randomMember.name + "</h1>");
-    $("#img-field").attr("src", randomMember.src);
-
-    // members.splice(randomMember, 1);
-    // console.log(members);
+    $("#img-div").html("<img id='img-field' src='images/" + randomMember.src + "'>");
 });
